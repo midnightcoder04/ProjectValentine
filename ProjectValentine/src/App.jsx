709b3544ebe3b8cd90ae93329dc8f096eebd63bd
Habeeb00 <122091247+Habeeb00@users.noreply.github.com>
2025-02-13 12:24:56 +0530

    testing upload

--- ProjectValentine/src/App.jsx
+++ ProjectValentine/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Quiz from "./pages/Quiz";
@@ -7,32 +7,32 @@ import Compare from "./pages/Compare";
 import About from "./pages/About";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <Router>
-      <div className="flex flex-col min-h-screen">
-        <NavBar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/quiz" element={<Quiz />} />
-            <Route path="/card" element={<Card />} />
-            <Route path="/compare" element={<Compare />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+        <div className="flex flex-col min-h-screen w-full bg-gray-900 text-white">
+          <NavBar />
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/quiz" element={<Quiz />} />
+              <Route path="/card" element={<Card />} />
+              <Route path="/compare" element={<Compare />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
